494dbb18136f26e22c59ed8cabee3e90b3152275
A DIONN <ashley.muldrow@yahoo.com>
2021-11-26 18:49:09 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksTodoEl = document.querySelector("#tasks-to-do");
+
+var createTaskhandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.classname = "task-item";
+    listItemEl.textcontent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+buttonEl.addEventListener("click", createTaskHandler); {
+
+};
+
+cd 
